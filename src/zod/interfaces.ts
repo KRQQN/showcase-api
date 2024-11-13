543524854a9982f3189abbf6a4470ac98017ad2a
krqqn <robin.krqqn@hotmail.com>
2024-11-13 20:35:13 +0100

    moved missplaced interface

--- src/zod/interfaces.ts
+++ src/zod/interfaces.ts
@@ -2,3 +2,12 @@ export interface ICustomError extends Error {
   statusCode: number;
   redirect: string;
 }
+
+export interface IRepository<T> {
+  getAll()                   : Promise<T[]>;
+  getById(id: number)        : Promise<T | null>;
+  create(item: T)            : Promise<T>;
+  update(id: number, item: T): Promise<T | null>;
+  delete(id: number)         : Promise<boolean>;
+}
+
