f4ac71f882c3dc564019d06caa93408ceb435e73
krqqn <robin.krqqn@hotmail.com>
2024-11-13 20:19:41 +0100

    loginRouterfixes

--- src/routes/loginRegisterRoutes.ts
+++ src/routes/loginRegisterRoutes.ts
@@ -1,35 +1,41 @@
 import { Request, Response, RequestHandler, Router } from 'express';
 import { userCredentialsSchema } from '../zod/schemas';
 import bcrypt from 'bcrypt';
-
-export async function registerHandler(req: Request, res: Response) {
-  const pareseRes = userCredentialsSchema.safeParse(req.body);
-
-  if (pareseRes.success) {
-    const pw = pareseRes.data.password;
-    const hashedPassword = await bcrypt.hash(pw, 10);
-    const userCredentials = { ...pareseRes.data, password: hashedPassword };
-    const user = await req.repository.create(userCredentials);
-
-    if (user) res.status(201).json({ message: 'User created' });
-    else throw new ServerError('Failed to create user');
-  } else res.status(400).json({ message: pareseRes.error });
-}
-
-export async function loginHandler(req: Request, res: Response) {
-  const pareseRes = userCredentialsSchema.safeParse(req.body);
-
-  if (pareseRes.success) {
-    const userCredentials = pareseRes.data;
-
-    const user = await req.repository.findOne({where: { email: userCredentials.email }});
-    const match = await bcrypt.compare(userCredentials.password, user.password);
-
-    if (user && match) res.status(200).json({ message: 'Login successful' });
-    else res.status(401).json({ message: 'Email or password not correct' });
-  } else res.status(400).json({ message: pareseRes.error });
-}
+import {
+  BadRequestException,
+  UnauthorizedException,
+  UserInputException
+} from '../utils/customErrors';
 
 export const loginRegisterRouter = Router()
-  .post('/register', registerHandler)
-  .post('/login', loginHandler);
+  .post('/register', (async (
+    req: Request,
+    res: Response
+  ): Promise<Response | void> => {
+
+    const pareseRes = userCredentialsSchema.safeParse(req.body);
+    if (!pareseRes.success) throw new UserInputException();
+
+    const pw = pareseRes.data.password;
+    const hashedPassword = await bcrypt.hash(pw, 10);
+    const userCredentials = { ...pareseRes.data, passwordHash: hashedPassword };
+    const user = await req.repository.create(userCredentials);
+
+    if (!user) throw new BadRequestException('Something went wrong creating user');
+    else return res.status(201).json({ message: 'User created' });
+  }) as RequestHandler)
+
+  .post('/login', (async (req: Request, res: Response): Promise<Response | void> => {
+
+    const pareseRes = userCredentialsSchema.safeParse(req.body)
+    if (!pareseRes.success) throw new UserInputException();
+
+    const userCredentials = pareseRes.data;
+    const user = await req.repository.findOne({
+      where: { email: userCredentials.email }
+    });
+    const match = await bcrypt.compare(userCredentials.password, user.passwordHash);
+
+    if (!user || !match) throw new UnauthorizedException();
+    return res.status(200).json({ message: 'Login successful' });
+  }) as RequestHandler);
