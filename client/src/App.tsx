8ef7c9748b3bd2904fe915a22acecda8d56a69b1
krqqn <robin.krqqn@hotmail.com>
2025-02-12 16:40:19 +0100

    React Router & app() cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,15 @@
 import './App.css';
-import { Box, Stack } from '@chakra-ui/react';
-import { useState } from 'react';
-import FrenchBulldog from './components/custom/frenchie';
-import TrNameLanguage from './components/custom/TypewriterNameLanguage';
-import WaveDivider from './components/layout/waveDivider';
-import Card from './components/custom/scroll';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Wordle from './pages/Wordle';
 
 function App() {
-  const [open, setOpen] = useState(false);
   return (
-    <>
-      <Box w={'100vw'} h={{smDown:'150vh', sm: '120vh', md:'200vh', lg:'200vh'}} className="pattern gradient-bg se-yellow ua-blue">
-        <FrenchBulldog />
-        <TrNameLanguage />
-      </Box>
-
-      <Stack gap={0} bg={'#0066cc'} h={'20rem'}>
-        <WaveDivider color="#FFD700" height="full" width="100vw" />
-
-        <Box
-          h={'100%'}
-          flex={'auto'}
-          justifyContent={'center'}
-          alignContent={'center'}
-          bg="linear-gradient(0deg, #242424 0%, #0066cc 80%)"
-        >
-          <Box bg="none" rounded={'xl'}  mt={'2rem'} zIndex={40}>
-            <Box h={'50rem'} pt={'10rem'}>
-              <Card/>
-            </Box>
-          </Box>
-        </Box>
-      </Stack>
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/wordle" element={<Wordle/>} />
+    </Routes>
   );
 }
-
-
 
 export default App;
