e6f732dff265151d2b953a07fe4cfaeff5601691
krqqn <robin.krqqn@hotmail.com>
2025-02-24 19:43:19 +0100

    homepage tweaking, experimenting, added parallax

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,39 +1,52 @@
 import FrenchBulldog from '@/components/custom/frenchie';
 import CardScrollList from '@/components/custom/cardScrollList';
 import TrNameLanguage from '@/components/custom/TypewriterNameLanguage';
-import WaveDivider from '@/components/layout/waveDivider';
-import { Box,  Stack } from '@chakra-ui/react';
+import { Box, Stack } from '@chakra-ui/react';
+import ColorFramedBox from '@/components/custom/colorFramedBox';
+import { motion } from 'framer-motion';
+import './home.scss';
 
 const Home = () => {
   return (
-    <>
-      <Box
-        w={'100vw'}
-        h={{ smDown: '150vh', sm: '120vh', md: '200vh', lg: '200vh' }}
-        className="pattern gradient-bg se-yellow ua-blue"
-      >
+    <Box className="bg_grad">
+      <Box w={'100vw'} h={{ base: '100vh', smDown: '130vh' }}>
         <FrenchBulldog />
         <TrNameLanguage />
       </Box>
 
-      <Stack gap={0} bg={'#0066cc'} h={'20rem'}>
-        <WaveDivider color="#FFD700" height="full" width="100vw" />
+
+      <Box w={'60%'} m={'auto'}>
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: -200, opacity: 1 }}
+          exit={{ y: 50, opacity: 0 }}
+          transition={{ duration: 1, ease: 'easeOut' }}
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <ColorFramedBox>
+            <Box>
+              <p>Hi, I'm a Full Stack Developer</p>
+            </Box>
+          </ColorFramedBox>
+        </motion.div>
+      </Box>
+
+
+      <Stack gap={0} h={'50rem'}>
+
 
         <Box
           h={'100%'}
           flex={'auto'}
           justifyContent={'center'}
           alignContent={'center'}
-          bg="linear-gradient(0deg, #242424 0%, #0066cc 80%)"
         >
-          <Box bg="none" rounded={'xl'} mt={'2rem'} zIndex={40}>
-            <Box h={'50rem'} pt={'10rem'}>
+
               <CardScrollList />
-            </Box>
-          </Box>
+
         </Box>
       </Stack>
-    </>
+    </Box>
   );
 };
 
